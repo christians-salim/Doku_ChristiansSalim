f05c606633f64aa8d34db68dfd13bc1d0f41d1de
Christians Salim <christians.salim@binus.edu>
2022-04-13 22:01:24 +0700

    first commit

---
+++ src/main/java/InterviewDoku/Doku_ChristiansSalim/DokuChristiansSalimApplication.java
@@ -0,0 +1,13 @@
+package InterviewDoku.Doku_ChristiansSalim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokuChristiansSalimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokuChristiansSalimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InterviewDoku/Doku_ChristiansSalim/DokuChristiansSalimApplicationTests.java
@@ -0,0 +1,13 @@
+package InterviewDoku.Doku_ChristiansSalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokuChristiansSalimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
